146f5a237b7cd283ccfec9e2ea1952f7deeee468
Subhankar Panda <pandasubhankar99@gmail.com>
2024-12-02 01:37:20 -0500

    add python

--- init.lua
+++ init.lua
@@ -618,6 +618,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        pylsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
