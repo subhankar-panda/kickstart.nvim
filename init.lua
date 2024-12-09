b0178b1cf80ff094b068d4a1864b14d05cf86838
Subhankar Panda <pandasubhankar99@gmail.com>
2024-12-09 11:44:34 -0500

    tabs n stuff

--- init.lua
+++ init.lua
@@ -110,6 +110,15 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.expandtab = true
+vim.opt.smartindent = false
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
